d708dd8a2c2592d30969686e7badd0d636f44d47
n4ame1 <128154091+n4ame1@users.noreply.github.com>
2024-01-07 21:08:19 +0500

    очень мало фиксов

--- MiniGames/RoomCleaning/game.gd
+++ MiniGames/RoomCleaning/game.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 @onready var washing_machine_game :Node2D = $Mashine_full
 @onready var hint :Label  =$Container/Label
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	washing_machine.input_event.connect(open_machine)
 	washing_machine.mouse_entered.connect(func(): print('hello!'))
@@ -20,7 +20,7 @@ func open_machine(viewport, event:InputEventMouseButton, shape_idx):
 		washing_machine.get_parent().visible=false
 		washing_machine.input_event.disconnect(open_machine)
 		hint.text= "Положи вещи в стиралку"
-		await get_tree().create_timer(1.0).timeout
+		await get_tree().create_timer(0.4).timeout
 		washing_machine_open.input_event.connect(load_machine)
 	
 func load_machine(viewport, event:InputEventMouseButton, shape_idx):
@@ -29,7 +29,7 @@ func load_machine(viewport, event:InputEventMouseButton, shape_idx):
 		washing_machine_open.get_parent().visible=false
 		washing_machine_open.input_event.disconnect(load_machine)
 		hint.text= "Закрой дверцу"
-		await get_tree().create_timer(1.0).timeout
+		await get_tree().create_timer(0.4).timeout
 		washing_machine_clothes.input_event.connect(close_machine)
 	
 func close_machine(viewport, event:InputEventMouseButton, shape_idx):
@@ -40,15 +40,12 @@ func close_machine(viewport, event:InputEventMouseButton, shape_idx):
 		hint.text= "А теперь крути баран по часов стрелке"
 		pregress_bar.max_value = washing_machine_game.MAX_NEED_ROTATION_DEGRESS
 		washing_machine_game.progress_bar = pregress_bar
-		await get_tree().create_timer(0.1).timeout
+		pregress_bar.show()
+		await get_tree().create_timer(0.4).timeout
 		washing_machine_game.process_mode = Node.PROCESS_MODE_PAUSABLE
 
 func finish_game():
-	await get_tree().create_timer(2).timeout
 	hint.text= "Готово"
+	await get_tree().create_timer(1).timeout
 	get_parent().finish_game()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 
