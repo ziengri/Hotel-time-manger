c4fd826803ee0afaa62c8f14f767f389c2d1ca76
ziengri <70845065+ziengri@users.noreply.github.com>
2024-01-07 18:51:47 +0300

    ОЧЕНЬ ММНОГО ФИКСОВ

--- MiniGames/base_game.gd
+++ MiniGames/base_game.gd
@@ -12,13 +12,6 @@ func finish_game()->void:
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
-
-func _on_button_pressed()->void:
-	finish_game()
